b9006062d4a8f43a5edaa7d0efdc06e2932c3602
wintermute28 <a28c@yandex.ru>
2023-11-09 12:39:10 +0300

    Edit modals

--- dist/script.js
+++ dist/script.js
@@ -17,33 +17,33 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll("[data-modal]"),
+      windows = document.querySelectorAll('[data-modal]'),
       scroll = calcScroll();
     trigger.forEach(item => {
-      item.addEventListener("click", e => {
+      item.addEventListener('click', e => {
         if (e.target) {
           e.preventDefault();
         }
         windows.forEach(item => {
-          item.style.display = "none";
+          item.style.display = 'none';
         });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
         document.body.style.marginRight = `${scroll}px`;
       });
     });
-    close.addEventListener("click", () => {
+    close.addEventListener('click', () => {
       windows.forEach(item => {
-        item.style.display = "none";
+        item.style.display = 'none';
       });
       modal.style.display = "none";
       document.body.style.overflow = "";
       document.body.style.marginRight = `0px`;
     });
-    modal.addEventListener("click", e => {
+    modal.addEventListener('click', e => {
       if (e.target === modal && closeClickOverlay) {
         windows.forEach(item => {
-          item.style.display = "none";
+          item.style.display = 'none';
         });
         modal.style.display = "none";
         document.body.style.overflow = "";
@@ -53,28 +53,36 @@ const modals = () => {
   }
   function showModalByTime(selector, time) {
     setTimeout(function () {
-      document.querySelector(selector).style.display = "block";
-      document.body.style.overflow = "hidden";
+      let display;
+      document.querySelectorAll('[data-modal]').forEach(item => {
+        if (getComputedStyle(item).display !== 'none') {
+          display = "block";
+        }
+      });
+      if (!display) {
+        document.querySelector(selector).style.display = 'block';
+        document.body.style.overflow = "hidden";
+      }
     }, time);
   }
   function calcScroll() {
-    let div = document.createElement("div");
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.overflowY = "scroll";
-    div.style.visibility = "hidden";
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
     document.body.appendChild(div);
-    //вычисляем ширину прокрутки
     let scrollWidth = div.offsetWidth - div.clientWidth;
     div.remove();
     return scrollWidth;
   }
-  bindModal(".button-design", ".popup-design", "popup-design .popup-close");
-
-  // showModalByTime(".popup", 6000);
+  bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+  bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  showModalByTime('.popup-consultation', 5000);
 };
-
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
+
+// bindModal(".button-design", ".popup-design", "popup-design .popup-close");
 
 /***/ })
 
