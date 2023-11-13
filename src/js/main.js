f7c5f1a31e93f8ca17dc00b2ced378934ce98bfe
wintermute28 <a28c@yandex.ru>
2023-11-13 12:44:29 +0300

    Add forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
     
     modals();
     sliders(".feedback-slider-item", "horizontal", ".main-prev-btn", ".main-next-btn");
-    sliders(".main-slider-item", "vertical")
+    sliders(".main-slider-item", "vertical");
+    forms();
 
 
 
