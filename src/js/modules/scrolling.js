0d76b2400b05f576082625b637de8d12e5e428d2
wintermute28 <a28c@yandex.ru>
2023-11-19 13:22:34 +0300

    Add smooth scroll

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -1,3 +1,4 @@
+//Up-btn
 const scrolling = (upSelector) => {
     const upElem = document.querySelector(upSelector);
 
@@ -10,6 +11,61 @@ const scrolling = (upSelector) => {
             upElem.classList.remove("fadeIn");
         }
     });
+//Smooth-scrool
+    const element = document.documentElement,
+          body = document.body;
+    
+    const calcScroll = () => {
+        upElem.addEventListener("click", function(event) {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+            if (this.hash !== "") {
+                event.preventDefault();
+                let hashElement = document.querySelector(this.hash),
+                    hashElementTop = 0;
+                
+                while (hashElement.offsetParent) {
+                    hashElementTop += hashElement.offsetTop;
+                    hashElement = hashElement.offsetParent;
+                }
+
+                hashElementTop = Math.round(hashElementTop);
+                smoothScroll(scrollTop, hashElementTop, this.hash);
+
+            }
+        })
+    };
+
+    const smoothScroll = (from, to, hash) => {
+        let timeInerval = 1,
+            prevScrollTop,
+            speed;
+        
+        if (to > from) {
+            speed = 30;
+        } else {
+            speed = -30;
+        }
+
+        let move = setInterval(function() {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+            if (
+                prevScrollTop === scrollTop ||
+                (to > from && scrollTop >= to) ||
+                (to < from && scrollTop <= to)
+            ) {
+                clearInterval(move);
+                history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, "") + hash);
+            } else {
+                body.scrollTop += speed;
+                element.scrollTop += speed;
+                prevScrollTop = scrollTop;
+            }
+        }, timeInerval);
+    };
+
+    calcScroll();
 };
 
 export default scrolling;
